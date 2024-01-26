8a6a8d63b0fd092279a04c66f7a0e8b359a70726
2pk03 <alo.alt@me.com>
2024-01-26 10:42:40 +0100

    .gitignore +.docusaurus and node_modules

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/alo/Development/wayang/incubator-wayang-website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/alo/Development/wayang/incubator-wayang-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/alo/Development/wayang/incubator-wayang-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/alo/Development/wayang/incubator-wayang-website/src/css/custom.css"),
-];
