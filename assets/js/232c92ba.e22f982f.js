b375c19a2da98e2fbb59d92790c5feb814e8d1b2
2pk03 <2pk03@users.noreply.github.com>
2024-04-09 07:48:58 +0000

    deploy: 90c4436e783b55ca883d4531b72e53ae20297a15

---
+++ assets/js/232c92ba.e22f982f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwayang_website=self.webpackChunkwayang_website||[]).push([[6974],{2725:a=>{a.exports=JSON.parse('{"label":"python","permalink":"/blog/tags/python","allTagsPath":"/blog/tags","count":1,"unlisted":false}')}}]);
